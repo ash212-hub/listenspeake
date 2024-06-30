3804921b294e9cd503bae72bb08a97fa92d45020
ashif shaik <ashifshaik000@gmail.com>
2024-06-30 08:23:11 +0530

    listen update

--- src/pages/Listen.jsx
+++ src/pages/Listen.jsx
@@ -4,6 +4,7 @@ function Listen() {
   return (
     <div>
       <h4 className='text-5xl mt-8'>speach to text</h4>
+      <h2>hi <i class="fa fa-thermometer-empty" aria-hidden="true"></i></h2>
     </div>
   )
 }
