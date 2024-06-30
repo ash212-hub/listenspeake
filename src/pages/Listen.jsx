c928a151d5c728c75b1d3086c44082ae4cdc6869
ashif shaik <ashifshaik000@gmail.com>
2024-06-30 08:12:46 +0530

    commit

---
+++ src/pages/Listen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Listen() {
+  return (
+    <div>
+      <h4 className='text-5xl mt-8'>speach to text</h4>
+    </div>
+  )
+}
+
+export default Listen
---
+++ src/pages/Speake.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Speake() {
+  return (
+    <div>
+      this is speake page
+    </div>
+  )
+}
+
+export default Speake
